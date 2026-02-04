efe49f0c08d927d52495c315fb2609af9506a2f5
abishar605 <abisha.r2023@vitstudent.ac.in>
2026-02-04 15:24:23 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello CI Project");
+        System.out.println("Hello CI Project updated");
     }
 }
